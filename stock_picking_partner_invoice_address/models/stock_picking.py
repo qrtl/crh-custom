6ddf32733656050b7ce902a9943ecead56296ca0
kakurai8 <80655511+kakurai8@users.noreply.github.com>
2022-03-08 18:33:28 +0900

    st1 [2183][ADD] stock_picking_partner_invoice_address (#9)
    [2183](https://www.quartile.co/web?#id=2183&action=771&model=project.task&view_type=form&menu_id=505)
    
    - add partner_invoice_id to stock_picking form, tree
    - add compute function of Boolean item wether partner_invoice_id = partner_shipping_id or not
    - add filter wether partner_invoice_id = partner_shipping_id or not

---
+++ stock_picking_partner_invoice_address/models/stock_picking.py
@@ -0,0 +1,25 @@
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    partner_invoice_id = fields.Many2one(
+        string="Invoice Address", related="sale_id.partner_invoice_id", store=True
+    )
+    is_identical_partners = fields.Boolean(
+        string="PartnerAdd = InvoiceAdd",
+        compute="_compute_is_identical_partners",
+        store=True,
+        help="Whether or not the trading partner address is the same as the Invoice Address.",
+    )
+
+    @api.multi
+    @api.depends("partner_id", "partner_invoice_id")
+    def _compute_is_identical_partners(self):
+        for pick in self:
+            if pick.partner_id == pick.partner_invoice_id:
+                pick.is_identical_partners = True
